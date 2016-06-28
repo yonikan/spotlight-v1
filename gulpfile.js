1c4b7db1fdaca2dc09347bd4188fba30ec35a4ff
yonikan <yonikan@gmail.com>
2016-06-28 19:49:32 +0300

    - setting up gulp with sass and browsersync only.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,34 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+// var sourcemaps = require('gulp-sourcemaps');
+
+// var autoprefixer = require('gulp-autoprefixer');
+// var minifycss = require('gulp-minify-css');
+// var rename = require('gulp-rename');
+// var concat = require('gulp-concat');
+// var uglify = require('gulp-uglify');
+
+
+
+// // Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./sass/**/*.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
+// Compile sass into CSS & auto-inject into browsers
+// gulp.task('sass', function () {
+//  return gulp.src('./sass/**/*.scss')
+//   .pipe(sourcemaps.init())
+//   .pipe(sass().on('error', sass.logError))
+//   .pipe(sourcemaps.write())
+//   .pipe(gulp.dest('css'));
+//   .pipe(browserSync.stream());
+// });
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -11,36 +37,11 @@ gulp.task('serve', ['sass'], function() {
         server: "./"
     });
 
-    // Watch .js files
-    gulp.watch('./js/*.js', ['scripts']);
-     // Watch .scss files
-    gulp.watch('./scss/*.scss', ['sass']);
-
+    gulp.watch('./sass/*.scss', ['sass']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-
-// Concatenate JS Files
-gulp.task('scripts', function() {
-   return gulp.src('./js/*.js')
-     .pipe(concat('main.js'))
-     .pipe(gulp.dest('bundle'))
-     .pipe(browserSync.stream());
-});
-
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("./scss/**/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("bundle"))
-        .pipe(browserSync.stream());
-});
-
-
 gulp.task('default', ['serve']);
-
-
 
 
 
---
+++ js/yoni.js
@@ -0,0 +1,3 @@
+
+
+var yoni = testing;
